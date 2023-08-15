6a0ff80709e09f541414d66c521b95473cdc8f83
Junel <robin.lennuj@gmail.com>
2023-08-15 10:15:53 -0700

    Added function to generate logo.svg file.

--- index.js
+++ index.js
@@ -26,3 +26,41 @@ const questions = [
         message: "Enter a color for your shape:",
     },
 ];
+
+async function run() {
+    try {
+        const answers = await inquirer.prompt(questions);
+
+        const text = answers.Text;
+        const textColor = answers["Text-Color"];
+        const shapeType = answers.Shape;
+        const shapeColor = answers["Shape-Color"];
+
+        let shape;
+        switch (shapeType) {
+            case "Triangle":
+                shape = new Triangle(shapeColor);
+                break;
+            case "Square":
+                shape = new Square(shapeColor);
+                break;
+            case "Circle":
+                shape = new Circle(shapeColor);
+                break;
+            default:
+                console.log("Invalid shape type");
+                return;
+        }
+
+        const svgLogo = generateSVGLogo(text, textColor, shape);
+
+        // Save the generated SVG logo to a file
+        fs.writeFileSync("logo.svg", svgLogo);
+
+        console.log("SVG logo generated and saved as 'logo.svg'");
+    } catch (error) {
+        console.error("Error occurred:", error);
+    }
+}
+
+run();
